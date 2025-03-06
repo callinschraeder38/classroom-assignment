b8ebb585ec48c3e16a58ecdbaf6a50d03fcaed60
callinschraeder38 <callinschraeder38@gmail.com>
2025-03-06 09:34:18 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,6 @@
+public class RandomNumber {
+  public static void main(String[] args) {
+    int random = (int)(Math.random()*10)+1;
+    System.out.println("Random number between 1 and 10: " + random);
+  }
+}
