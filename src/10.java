c6cc5965ef8cac3bae9f7a89aa7861f1b36850b3
callinschraeder38 <callinschraeder38@gmail.com>
2025-03-15 05:12:14 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Assignment {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 8;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+    }
+}
