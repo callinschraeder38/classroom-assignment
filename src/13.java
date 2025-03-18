828be3138a58cbc48f03cc02447ae0dc5ff4749a
callinschraeder38 <callinschraeder38@gmail.com>
2025-03-18 02:19:56 +0000

    Add 13.java

---
+++ src/13.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class CollaborativeProject {
+   public static void main(String[] args) {
+       System.out.println("Hello World!");
+       Random random = new Random();
+       int n = 1 + random.nextInt(10); // generate a number between 1 and 10
+       System.out.println(n);
+   }
+}
