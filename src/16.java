4395c655674494b2145eb90dbe06b6fedf0bec84
callinschraeder38 <callinschraeder38@gmail.com>
2025-03-22 16:18:37 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,29 @@
+public class ClassroomAssignment {
+    public static void main(String[] args) {
+        // Example 1: Simple addition of two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum is: " + (num1 + num2));
+        
+        // Example 2: Displaying a message with an action
+        String actionMessage = "Hello, this is an example.";
+        System.out.println(actionMessage);
+        
+        // Example 3: Reading and printing integers from standard input
+        int number1 = 42;
+        int number2 = 7;
+        int result = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + result);
+        
+        // Example 4: Reading and printing floating-point numbers from standard input
+        double number3 = 6.5;
+        double number4 = 7.0;
+        double result2 = number3 * number4;
+        System.out.println("The product of " + number3 + " and " + number4 + " is: " + result2);
+        
+        // Example 5: Displaying a message with an action and two numbers
+        String actionMessage1 = "Greetings, this is the first action.";
+        int number5 = 8;
+        System.out.println(actionMessage1);
+    }
+}
