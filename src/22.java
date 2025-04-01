b339c4642d3ebdb4eb6435195ff5f954694ea4f7
callinschraeder38 <callinschraeder38@gmail.com>
2025-04-01 16:00:08 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,6 @@
+public class ClassroomAssignment {
+    public static void main(String[] args) {
+        // Example of a simple Java program with command line arguments
+        System.out.println("Hello World!");
+    }
+}
