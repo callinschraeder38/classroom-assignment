efb3294ce2ead7456ab595fa040ef8817ac1c398
callinschraeder38 <callinschraeder38@gmail.com>
2025-04-02 08:36:11 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,15 @@
+public class ClassroomAssignment {
+
+    // Method to perform some operation on the input
+    public void executeOperation(String userInput) {
+        System.out.println("Processing user input: " + userInput);
+        // Add your implementation here
+    }
+
+    // Main method to run the assignment
+    public static void main(String[] args) {
+        ClassroomAssignment assignment = new ClassroomAssignment();
+        String userInput = "Hello, how are you?";
+        assignment.executeOperation(userInput);
+    }
+}
