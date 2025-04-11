4b46ca07f5ef193225c27c0abf3d4ab0da08eca9
callinschraeder38 <callinschraeder38@gmail.com>
2025-04-11 10:44:12 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,11 @@
+public class ClassroomAssignment {
+    public static void main(String[] args) {
+        // Example of using if-else statement in Java
+        int age = 18;
+        if (age >= 18 && age <= 25) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a teenager or younger.");
+        }
+    }
+}
