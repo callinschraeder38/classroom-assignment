4e4124c3e180c6f7602e686cf35803ca6997766b
callinschraeder38 <callinschraeder38@gmail.com>
2025-04-23 11:32:33 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,12 @@
+public class ClassroomAssignment {
+    public static void main(String[] args) {
+        // Simulate student input
+        String input = "Hello, World!";
+        
+        if (input.contains("World!")) {
+            System.out.println("Word found!");
+        } else {
+            System.out.println("No match.");
+        }
+    }
+}
