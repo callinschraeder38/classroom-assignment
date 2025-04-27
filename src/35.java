1b92e4b7f79f01991fbe99f03d7d26efec2595e4
callinschraeder38 <callinschraeder38@gmail.com>
2025-04-27 02:43:36 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,19 @@
+public class ClassroomAssignment {
+
+    public void mainMethod() {
+        // Example of student input and output
+        System.out.println("Hello, how are you?");
+        
+        int grade = 85;
+        if (grade >= 90) {
+            System.out.println("You got an A!");
+        } else if (grade >= 70) {
+            System.out.println("You got a B.");
+        } else if (grade >= 60) {
+            System.out.println("You're doing well, but you need to work hard next semester.");
+        } else {
+            System.out.println("You failed this class!");
+        }
+    }
+
+}
