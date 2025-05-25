b59affb889bc20c1d5316121c71fb72a648a920a
callinschraeder38 <callinschraeder38@gmail.com>
2025-05-25 23:54:01 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,17 @@
+public class CollaborativeProject {
+    public static void main(String[] args) {
+        // Example of using loops and conditionals in Java
+
+        // For loop example
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Looping through the numbers from 0 to " + (i + 1));
+        }
+
+        // While loop example
+        int x = 5;
+        while (x > 0) {
+            System.out.println(x);
+            x--;
+        }
+    }
+}
