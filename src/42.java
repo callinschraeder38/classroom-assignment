098f02eb61a6c29c2653ab85fd8379319d0f639f
callinschraeder38 <callinschraeder38@gmail.com>
2025-05-26 08:41:11 +0000

    Add 42.java

---
+++ src/42.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    public static void main(String[] args) {
+        // Example code to demonstrate basic Java functionality
+        System.out.println("Hello, World!");
+        
+        int x = 5;
+        double y = 3.14;
+        
+        if (x > 0) {
+            System.out.println("X is positive.");
+        } else {
+            System.out.println("X is not positive.");
+        }
+    }
+}
